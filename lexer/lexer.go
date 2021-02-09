cafd4f6a57122d122efd048aa66b5b19a19912e9
kdnakt <a.kid.1985@gmail.com>
2021-02-09 19:59:55 +0900

    1.4 extend tokens

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -4,10 +4,10 @@ import "kdnakt/writing-an-interpreter-in-go/token"
 
 // Lexer analyes input and returns token
 type Lexer struct {
-	input		 string
-	position	 int
+	input        string
+	position     int
 	readPosition int
-	ch			 byte
+	ch           byte
 }
 
 // New creates new Lexer
@@ -35,7 +35,33 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '!':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
@@ -91,6 +117,14 @@ func (l *Lexer) readNumber() string {
 		l.readChar()
 	}
 	return l.input[pos:l.position]
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
 
 func isLetter(ch byte) bool {
