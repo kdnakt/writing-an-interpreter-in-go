6c02d5fb7f25f8437ce50edba2cecb438b5e1a7b
kdnakt <a.kid.1985@gmail.com>
2021-02-11 16:11:20 +0900

    2.5. return statement

--- ast/ast.go
+++ ast/ast.go
@@ -26,6 +26,7 @@ type Program struct {
 	Statements []Statement
 }
 
+// TokenLiteral returns literal value
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -34,6 +35,7 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+// LetStatement is `let ...`
 type LetStatement struct {
 	Token token.Token // token.LET
 	Name *Identifier
@@ -41,16 +43,29 @@ type LetStatement struct {
 }
 
 func (ls *LetStatement) statementNode() {}
+// TokenLiteral returns literal value
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+// ReturnStatement is `return ...`
+type ReturnStatement struct {
+	Token		token.Token
+	ReturnValue	Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+// TokenLiteral returns literal value
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+// Identifier is a symbol
 type Identifier struct {
 	Token token.Token // token.IDENT
 	Value string
 }
 
 func (i *Identifier) expressionNode() {}
+// TokenLiteral returns literal value
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
